5e4209b6034b47c9b1b763b2e998c57146343e16
jailsonveiga <jmdaveiga2012@gmail.com>
2021-12-18 23:08:44 -0500

    Practice_02 / 8 Primitive Data Types

---
+++ src/Practice_02.java
@@ -0,0 +1,14 @@
+public class Practice_02 {
+    public static void main(String[] args) {
+        byte myByte = 120;
+        short myShort = 330;
+        int myInt = 200;
+        long myLong = 1500;
+        float myFloat = 5.98F;
+        double myDouble = 3.14D;
+        char myChar = 'C';
+        boolean myBoolean = false;
+
+        System.out.println(myChar + "-" + myBoolean + '\n' + myByte + '\n' + myInt + '\n' + myShort + '\n' + myLong + '\n' + myDouble + '\n' + myFloat + '\n');
+    }
+}
