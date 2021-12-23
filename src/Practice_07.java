d826715bea37611a341af9f346e8348c1d35be7b
jailsonveiga <jmdaveiga2012@gmail.com>
2021-12-22 23:59:26 -0500

    Control Statements / If else Statement

---
+++ src/Practice_07.java
@@ -0,0 +1,22 @@
+// Control Statements / If else Statement
+
+public class Practice_07 {
+
+    public static void main(String[] args) {
+       int age = 19;
+
+       if(age >= 21) {
+
+           System.out.println("You can have a drink");
+
+       }// end if
+
+       else {
+
+           System.out.println("You cannot have a drink yet");
+
+       }//end else
+
+    }//end main
+
+}//end class
