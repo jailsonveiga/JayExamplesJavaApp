e17d8e7d2f7263e745f17dff31496911b504e4f6
jailsonveiga <jmdaveiga2012@gmail.com>
2022-01-22 23:19:29 -0500

    Making and testing Dog objects

---
+++ src/classes_objects/Dog.java
@@ -0,0 +1,13 @@
+package classes_objects;
+
+public class Dog {
+    // instance variable
+    int size;
+    String breed;
+    String name;
+
+    // method
+    void bark() {
+        System.out.println("Ruff! Ruff!");
+    }
+}
