d0398110caa8c0117e01f858bb712e03787565ae
jailsonveiga <jmdaveiga2012@gmail.com>
2022-01-22 23:18:44 -0500

    Making and testing Movie objects

---
+++ src/classes_objects/Movie.java
@@ -0,0 +1,11 @@
+package classes_objects;
+
+public class Movie {
+    String title;
+    String genre;
+    int rating;
+
+    void playIt() {
+        System.out.println("Playing the movies");
+    }
+}
