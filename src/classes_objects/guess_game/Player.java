9389cc80392ab1f69048735c30795d49583a74f7
jailsonveiga <jmdaveiga2012@gmail.com>
2022-02-06 21:47:27 -0500

    Player.java

---
+++ src/classes_objects/guess_game/Player.java
@@ -0,0 +1,10 @@
+package classes_objects.guess_game;
+
+public class Player {
+    int number = 0;
+
+    public void guess() {
+        number = (int) (Math.random() * 10);
+        System.out.println("I'm guessing " + number);
+    }
+}
