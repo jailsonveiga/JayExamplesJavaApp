e07e01e71a9608194655a8ccafedc1997093cc9a
jailsonveiga <jmdaveiga2012@gmail.com>
2022-02-07 21:46:11 -0500

    GameLauncher.java

---
+++ src/classes_objects/guess_game/GameLauncher.java
@@ -0,0 +1,8 @@
+package classes_objects.guess_game;
+
+public class GameLauncher {
+    public static void main(String[] args) {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+}
