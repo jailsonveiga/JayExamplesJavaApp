d8923bf1f8febd996bb3519844cc1c9e4ce85d8a
jailsonveiga <jmdaveiga2012@gmail.com>
2022-03-09 17:23:00 -0500

    Keyboard Input - Java

--- src/keyboard_input/KeyboardInput.java
+++ src/keyboard_input/KeyboardInput.java
@@ -1,4 +1,19 @@
 package keyboard_input;
 
+import java.util.Scanner;
+
 public class KeyboardInput {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter your name?");
+
+        String userInput = keyboard.nextLine();
+        System.out.println("My name is: " + userInput);
+
+        System.out.println("How old are you?");
+
+        int userInputOld = keyboard.nextInt();
+        System.out.println("I am: " + userInputOld + " years age");
+
+    }
 }
