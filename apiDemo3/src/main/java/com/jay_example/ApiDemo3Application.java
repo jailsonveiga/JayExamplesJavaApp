27d90b43b7a24e4472c25c76b1814b79ea15eb59
jailsonveiga <jmdaveiga2012@gmail.com>
2022-03-12 12:07:34 -0500

    API Demo 3 ApiDemo3Application.java

---
+++ apiDemo3/src/main/java/com/jay_example/ApiDemo3Application.java
@@ -0,0 +1,13 @@
+package com.jay_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDemo3Application.class, args);
+	}
+
+}
