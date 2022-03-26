20d487a191f36af6b899c4f300a70a7f7b4b4b5c
jailsonveiga <jmdaveiga2012@gmail.com>
2022-03-26 14:46:15 -0400

    First API Created for the intent to connect to the JSON PlaceHolder API JsonPlaceHolderApiApplicationTests.java

---
+++ JsonPlaceHolderAPI/src/test/java/com/example/demo/JsonPlaceHolderAPI/JsonPlaceHolderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.JsonPlaceHolderAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonPlaceHolderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
