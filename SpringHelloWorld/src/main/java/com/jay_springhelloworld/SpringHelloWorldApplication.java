14c1a902c58c326601618f9bb661c86845dad2b4
jailsonveiga <jmdaveiga2012@gmail.com>
2022-04-13 13:48:59 -0400

    Demo Project for Spring Boot SpringHelloWorldApplication.java

--- SpringHelloWorld/src/main/java/com/jay_springhelloworld/SpringHelloWorldApplication.java
+++ SpringHelloWorld/src/main/java/com/jay_springhelloworld/SpringHelloWorldApplication.java
@@ -2,6 +2,8 @@ package com.jay_springhelloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringHelloWorldApplication {
@@ -10,4 +12,8 @@ public class SpringHelloWorldApplication {
 		SpringApplication.run(SpringHelloWorldApplication.class, args);
 	}
 
+		@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+		}
 }
