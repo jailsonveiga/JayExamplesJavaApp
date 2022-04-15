5fa8c1b6b6bd99a4fe15cb198f69698e97eb144b
jailsonveiga <jmdaveiga2012@gmail.com>
2022-04-15 09:41:22 -0400

    GoRest Gateway GorestApplication.java

---
+++ Gorest/src/main/java/com/jay_example_gorest/GorestApplication.java
@@ -0,0 +1,13 @@
+package com.jay_example_gorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GorestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GorestApplication.class, args);
+	}
+
+}
