6fadd49295326edc4f9802fc56ff6d9850825277
jailsonveiga <jmdaveiga2012@gmail.com>
2022-04-15 09:38:15 -0400

    GoRest Gateway maven-wrapper.jar

---
+++ Gorest/src/test/java/com/jay_example_gorest/GorestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay_example_gorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GorestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
