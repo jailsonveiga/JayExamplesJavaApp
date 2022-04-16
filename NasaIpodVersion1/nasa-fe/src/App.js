ac65c216a8e89e272115848cc9ea5bc4d6b824c6
jailsonveiga <jmdaveiga2012@gmail.com>
2022-04-16 09:49:21 -0400

    Nasa Apod Version 1 App.js

---
+++ NasaIpodVersion1/nasa-fe/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ApodForm from "./components/ApodForm";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ApodForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
