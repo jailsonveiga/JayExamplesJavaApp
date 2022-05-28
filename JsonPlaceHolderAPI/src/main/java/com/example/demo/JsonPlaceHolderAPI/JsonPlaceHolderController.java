9f4e75a5e54b1c8698453c71cd2558a0c951f118
jailsonveiga <jmdaveiga2012@gmail.com>
2022-05-28 16:17:38 -0400

    First API Created for the intent to connect to the JSON PlaceHolder API JsonPlaceHolderController.java

--- JsonPlaceHolderAPI/src/main/java/com/example/demo/JsonPlaceHolderAPI/JsonPlaceHolderController.java
+++ JsonPlaceHolderAPI/src/main/java/com/example/demo/JsonPlaceHolderAPI/JsonPlaceHolderController.java
@@ -1,4 +1,18 @@
 package com.example.demo.JsonPlaceHolderAPI;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class JsonPlaceHolderController {
+
+    @RequestMapping("/")
+    public String helloWorld() {
+        return "Hello from Spring Boot";
+    }
+
+    @RequestMapping("/goodbye")
+    public String helloGoodBye() {
+        return "Goodbye from Spring Boot";
+    }
 }
