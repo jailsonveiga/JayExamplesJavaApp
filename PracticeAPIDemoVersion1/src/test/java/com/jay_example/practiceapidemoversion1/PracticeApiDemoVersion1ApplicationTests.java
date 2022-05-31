cca5e5e31ce67d724382a278fcea521192bee487
jailsonveiga <jmdaveiga2012@gmail.com>
2022-05-31 12:43:19 -0400

    Practice API Demo Version 1 PracticeApiDemoVersion1ApplicationTests.java

---
+++ PracticeAPIDemoVersion1/src/test/java/com/jay_example/practiceapidemoversion1/PracticeApiDemoVersion1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay_example.practiceapidemoversion1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApiDemoVersion1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
