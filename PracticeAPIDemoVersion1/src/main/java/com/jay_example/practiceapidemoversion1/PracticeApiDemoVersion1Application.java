bd4bcce300971e6e15525400eb150cef885275e1
jailsonveiga <jmdaveiga2012@gmail.com>
2022-05-31 12:43:06 -0400

    Practice API Demo Version 1 PracticeApiDemoVersion1Application.java

---
+++ PracticeAPIDemoVersion1/src/main/java/com/jay_example/practiceapidemoversion1/PracticeApiDemoVersion1Application.java
@@ -0,0 +1,13 @@
+package com.jay_example.practiceapidemoversion1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeApiDemoVersion1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeApiDemoVersion1Application.class, args);
+	}
+
+}
