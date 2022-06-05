f7af7597b220acd66f2f2429737e459c9295139a
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-05 12:12:41 -0400

    Nasa Ipod Version 1 NasaIpodVersion1Application.java

---
+++ NasaIpodVersion1/src/main/java/com/jay_nasaipod_version1/NasaIpodVersion1Application.java
@@ -0,0 +1,13 @@
+package com.jay_nasaipod_version1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaIpodVersion1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaIpodVersion1Application.class, args);
+	}
+
+}
