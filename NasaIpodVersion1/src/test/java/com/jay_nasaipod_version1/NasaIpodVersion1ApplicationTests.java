3d2f282227947009a5bed184906d5f78a9693cbf
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-05 12:12:55 -0400

    Nasa Ipod Version 1 NasaIpodVersion1ApplicationTests.java

---
+++ NasaIpodVersion1/src/test/java/com/jay_nasaipod_version1/NasaIpodVersion1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay_nasaipod_version1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaIpodVersion1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
