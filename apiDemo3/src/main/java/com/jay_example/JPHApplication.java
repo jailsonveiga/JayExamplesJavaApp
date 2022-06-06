0868b815f436cc915a5a0e3a469a407c373a5fcc
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-06 11:13:59 -0400

    API Demo 3 JPHApplication.java

--- apiDemo3/src/main/java/com/jay_example/JPHApplication.java
+++ apiDemo3/src/main/java/com/jay_example/JPHApplication.java
@@ -3,22 +3,22 @@ package com.jay_example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-//Particular class to handle request we use @RestController
 @RestController
 public class JPHApplication {
+	@RequestMapping("/ping")
+	public String pong () {
+		return "pong";
+	}
 
-	//
 	@GetMapping("/")
     public String rootRoute () {
-
 		return "you are home";
-
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(JPHApplication.class, args);
 	}
-
 }
