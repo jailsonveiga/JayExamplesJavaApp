7862648bf36b59e5898990b2290446d526156b9f
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-07 22:32:46 -0400

    Demo Project for Spring Boot Post.java

--- SpringHelloWorld/src/main/java/com/jay_springhelloworld/Post.java
+++ SpringHelloWorld/src/main/java/com/jay_springhelloworld/Post.java
@@ -2,4 +2,15 @@ package com.jay_springhelloworld;
 
 public class Post {
 
+    private int id;
+    private String title;
+
+    public Post(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Post() {
+
+    }
 }
